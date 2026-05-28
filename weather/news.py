2be86b1829d08a34bbffe41950795cfa9af18040
jchandd <jaydenc243@deltalearns.ca>
2026-05-27 19:59:17 -0700

    added API keys

--- weather/news.py
+++ weather/news.py
@@ -1,2 +1,9 @@
 # News and Weather API
 # After entering in an API, the program will give you back News and Weather in your area
+
+import requests
+
+
+weather_key = "cc96d68c73cf139304a381905f85826e" 
+news_key = "0ab27a701b0940ccbf34abef718528f7" 
+
