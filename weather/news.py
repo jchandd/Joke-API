2c164c6aad4f37fe2d502eada1065f1e8e1754ba
jchandd <jaydenc243@deltalearns.ca>
2026-05-27 20:01:37 -0700

    added in city weather

--- weather/news.py
+++ weather/news.py
@@ -7,3 +7,11 @@ import requests
 weather_key = "cc96d68c73cf139304a381905f85826e" 
 news_key = "0ab27a701b0940ccbf34abef718528f7" 
 
+city = input("City: ")
+
+weather = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_key}&units=metric").json()
+
+print("\nWeather:")
+print(weather["weather"][0]["description"])
+print(str(weather["main"]["temp"]) + "°C")
